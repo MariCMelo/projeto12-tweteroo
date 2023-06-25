95a23c40bdb453fd4c76c1ff522c26ff8a9ff263
Marilia <marilia.correa.melo@gmail.com>
2023-06-25 17:22:18 -0300

    App Workking - Loking for any problem

--- src/app.js
+++ src/app.js
@@ -5,20 +5,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const users = [
-    {
-        username: "Gato",
-        avatar: "https://www.shutterstock.com/image-vector/hand-drawn-vintage-cute-cat-260nw-1701332881.jpg"
-    }
-]
+const users = []
 
-const tweets = [
-    {
-        username: "Gato",
-        tweet: "Miau",
-        avatar: "https://www.shutterstock.com/image-vector/hand-drawn-vintage-cute-cat-260nw-1701332881.jpg"
-    }
-]
+const tweets = []
 
 app.post("/sign-up", (req, res) => {
     const { username, avatar } = req.body;
@@ -29,13 +18,12 @@ app.post("/sign-up", (req, res) => {
 
     const newUser = { username, avatar };
     users.push(newUser);
-    console.log("testedddd")
     res.sendStatus(201);
 });
 
 app.post("/tweets", (req, res) => {
-    const {username, tweet} = req.body
-
+    const { username, tweet} = req.body
+  
     const user = users.find(user => user.username === username)
     if (!user) {
         return res.status(401).send({ message: "UNAUTHORIZED" });
@@ -43,21 +31,22 @@ app.post("/tweets", (req, res) => {
     if (!tweet) {
         return res.status(422).send({ message: "Todos os campos são obrigatórios!" });
     }
-    const newTweet = { username, tweet };
-  tweets.push(newTweet)
-  res.sendStatus(201)
+    const newTweet = { username, tweet};
+    tweets.push(newTweet)
+    res.sendStatus(201)
 })
 
 app.get("/tweets", (req, res) => {
-    const recentTweets = tweets.slice(-10); // Retorna os 10 últimos tweets publicados
-
-    if (recentTweets.length === 0) {
-        return res.send([]);
-    }
+    const recentTweets = tweets.slice(-10).map(tweet => {
+        const user = users.find(user => user.username === tweet.username);
+        return {
+            ...tweet,
+            avatar: user ? user.avatar : null
+        };
+    });
 
     res.send(recentTweets);
 });
-
 
 const PORT = 5000;
 app.listen(PORT, () => console.log(`Servidor está rodando na porta ${PORT}`));
