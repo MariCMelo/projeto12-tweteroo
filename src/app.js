3648a06c16511e474242976d4ec280f27719e639
Marilia <marilia.correa.melo@gmail.com>
2023-06-25 15:23:58 -0300

    Testing posting tweet

--- src/app.js
+++ src/app.js
@@ -5,14 +5,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const user = [
+const users = [
     {
         username: "Gato",
         avatar: "https://www.shutterstock.com/image-vector/hand-drawn-vintage-cute-cat-260nw-1701332881.jpg"
     }
 ]
 
-const tweet = [
+const tweets = [
     {
         username: "Gato",
         tweet: "Miau",
@@ -20,24 +20,31 @@ const tweet = [
     }
 ]
 
-app.get("/tweets/:username", (req, res) => {
-    const { username } = req.params;
-    const userTweets = tweet.filter((t) => t.username === username);
-    res.send(userTweets);
-});
-
-
 app.post("/sign-up", (req, res) => {
     const { username, avatar } = req.body;
 
     if (!username || !avatar) {
-        return res.status(422).send({ message: "Todos os campos são obrigatórios!!!" });
+        return res.status(422).send({ message: "Todos os campos são obrigatórios!" });
     }
 
     const newUser = { username, avatar };
-    user.push(newUser);
+    users.push(newUser);
     res.sendStatus(201);
 });
+
+app.post("/tweets", (req, res) => {
+    const {username, tweet} = req.body
+
+    const user = users.find(user => user.username === username)
+    if (!user) {
+        return res.status(401).send({ message: "UNAUTHORIZED" });
+    }
+    if (!tweet) {
+        return res.status(422).send({ message: "Todos os campos são obrigatórios!" });
+    }
+    const newTweet = { username, tweet };
+  tweets.push(newTweet);
+})
 
 const PORT = 5000;
 app.listen(PORT, () => console.log(`Servidor está rodando na porta ${PORT}`));
